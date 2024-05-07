2607def26954c64d20c9d2213a01ff4ec569e550
JoseSalasCalderon <joseant2302@gmail.com>
2024-05-07 14:22:09 -0600

    Alexa Controller demo v1
    El formato de la respuesta es funcional

---
+++ C07051_B92291_Tarea_III_IF4101/Tarea3.Api/Controllers/AlexaController.cs
@@ -0,0 +1,23 @@
+﻿using Alexa.NET.Request;
+using Alexa.NET.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea3.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlexaController : ControllerBase
+    {
+        [HttpPost, Route("/process")]
+        public SkillResponse Process(SkillRequest input) {
+            
+            SkillResponse output = new SkillResponse();
+            output.Version = "1.0";
+            output.Response = new ResponseBody();
+            output.Response.OutputSpeech = new PlainTextOutputSpeech("Hola, esto funciona");
+
+            return output;
+        }
+    }
+}
